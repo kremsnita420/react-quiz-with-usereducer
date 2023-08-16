db3c8bc048a83a4967740f50aab4a6e59cf9027e
Safet Duranovic <duranovic.safet@gmail.com>
2023-08-16 12:49:43 +0200

    first commit

---
+++ src/components/Question.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import QuestionOptions from './QuestionOptions';
+
+export default function Question({ question, dispatch, answer }) {
+	return (
+		<div>
+			<h4>{question.question}</h4>
+			<QuestionOptions
+				question={question}
+				dispatch={dispatch}
+				answer={answer}
+			/>
+		</div>
+	);
+}
---
+++ src/components/QuestionOptions.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default function QuestionOptions({ question, dispatch, answer }) {
+	const hasAnswered = answer !== null;
+	return (
+		<div className='options'>
+			{question.options.map((option, index) => (
+				<button
+					key={option}
+					disabled={hasAnswered}
+					onClick={() => dispatch({ type: 'newAnswer', payload: index })}
+					className={`btn btn-option ${index === answer ? 'answer' : ''} ${
+						hasAnswered
+							? index === question.correctOption
+								? 'correct'
+								: 'wrong'
+							: ''
+					}`}>
+					{option}
+				</button>
+			))}
+		</div>
+	);
+}
